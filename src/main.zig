1fa73b6ea1ac2cfc1ac6b9344453a54cc6a6ba34
Derek Ko <koderek000@gmail.com>
2026-03-18 10:54:18 -0700

    add cat-file blob reader

--- src/main.zig
+++ src/main.zig
@@ -27,5 +27,58 @@ pub fn main() !void {
             _ = try head.write("ref: refs/heads/main\n");
         }
         try stdout.writeAll("Initialized git directory\n");
+        return;
     }
+
+    if (std.mem.eql(u8, command, "cat-file")) {
+        if (args.len != 4 or !std.mem.eql(u8, args[2], "-p")) {
+            return error.InvalidArguments;
+        }
+
+        try printBlob(allocator, args[3]);
+        return;
+    }
+}
+
+fn printBlob(allocator: std.mem.Allocator, object_hash: []const u8) !void {
+    if (object_hash.len != 40) {
+        return error.InvalidObjectHash;
+    }
+
+    var object_path_buffer: [std.fs.max_path_bytes]u8 = undefined;
+    const object_path = try std.fmt.bufPrint(
+        &object_path_buffer,
+        ".git/objects/{s}/{s}",
+        .{ object_hash[0..2], object_hash[2..] },
+    );
+
+    const cwd = std.fs.cwd();
+    const object_file = try cwd.openFile(object_path, .{});
+    defer object_file.close();
+
+    const compressed_object = try object_file.readToEndAlloc(allocator, std.math.maxInt(usize));
+    defer allocator.free(compressed_object);
+
+    var object_reader: std.Io.Reader = .fixed(compressed_object);
+    var decompressor: std.compress.flate.Decompress = .init(&object_reader, .zlib, &.{});
+    var decompressed_object: std.ArrayList(u8) = .empty;
+    defer decompressed_object.deinit(allocator);
+    try decompressor.reader.appendRemainingUnlimited(allocator, &decompressed_object);
+
+    const header_end = std.mem.indexOfScalar(u8, decompressed_object.items, 0) orelse {
+        return error.InvalidObject;
+    };
+
+    const header = decompressed_object.items[0..header_end];
+    if (!std.mem.startsWith(u8, header, "blob ")) {
+        return error.UnsupportedObjectType;
+    }
+
+    const content = decompressed_object.items[header_end + 1 ..];
+    const content_size = try std.fmt.parseInt(usize, header["blob ".len..], 10);
+    if (content.len != content_size) {
+        return error.InvalidObject;
+    }
+
+    try stdout.writeAll(content);
 }
